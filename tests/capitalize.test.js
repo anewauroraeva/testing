88e4f099a36e33c974d1622a70b9aaaf96f60e38
Eugenia <mbp@MacBook-Pro-MBP.Dlink>
2023-02-13 18:48:47 +0300

    add assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,11 @@
+import { strict as assert } from 'node:assert';
+
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize('hello'), 'Hello');
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+assert.equal(capitalize(''), '');
+
+assert.equal(capitalize('papi'), 'Papi');
 
 console.log('Все тесты пройдены!');
